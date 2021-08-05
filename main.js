c4e0473ed5c6e312734970fd98d51f6b5c9bb8a5
bhargavumarale <79691733+bhargavumarale@users.noreply.github.com>
2021-08-05 19:42:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+difference = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(450, 450);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+         console.log(results);
+         leftWristX = results[0].pose.leftWrist.x;
+         rightWristX = results[0].pose.rightWrist.x;
+         difference = floor(leftWristX - rightWristX);
+         console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + "difference" + difference);
+    }
+}
+
+function draw()
+{
+    document.getElementById("square_side").innerHTML = "Font size of the text will be = " + difference + "px";
+    background('#6C91C2');
+
+    textSize(difference);
+    fill('#FFE787')
+    text('Bhargav', leftWristX, rightWristX)
+}
